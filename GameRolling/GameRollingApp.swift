41074c36df31b2bb42e13599decd0a96eb51f578
Mirdinic <mirdin.sadykov@mail.ru>
2021-08-16 21:31:47 +0600

    Initial Commit

---
+++ GameRolling/GameRollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameRollingApp.swift
+//  GameRolling
+//
+//  Created by Mirdin Sadykov on 8/16/21.
+//
+
+import SwiftUI
+
+@main
+struct GameRollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
